e44bf5445b0ae6b3970291a33a5c148969a8d992
Kate <45145025+Python-dev-1@users.noreply.github.com>
2021-10-02 14:05:14 -0400

    Create Window_capture.py

---
+++ Window_capture.py
@@ -0,0 +1,30 @@
+import numpy as np
+import win32gui
+import win32ui
+import win32con
+
+def window_capture(x, y, w, h):
+
+
+    hwnd = win32gui.FindWindow(None, "LDPlayer")
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0, 0), (w, h), dcObj, (x, y), win32con.SRCCOPY)
+
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype="uint8")
+    img.shape = (h, w, 4)
+    img = img[...,:3]
+    img = np.ascontiguousarray(img)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+    return img

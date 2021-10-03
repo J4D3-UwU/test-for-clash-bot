e7ed4e872057dd6468456c2898cbec1032f28220
Kate <45145025+Python-dev-1@users.noreply.github.com>
2021-10-02 21:34:50 -0400

    Update Window_capture.py

--- Window_capture.py
+++ Window_capture.py
@@ -1,8 +1,10 @@
 import numpy as np
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
 import win32gui, win32ui, win32con
+import cv2 as cv
 
-def WindowCapture(x, y, w, h):
-
+def window_capture(x, y, w, h):
 
     hwnd = win32gui.FindWindow(None, "LDPlayer")
     wDC = win32gui.GetWindowDC(hwnd)
@@ -26,3 +28,7 @@ def WindowCapture(x, y, w, h):
     win32gui.DeleteObject(dataBitMap.GetHandle())
 
     return img
+
+cv.imshow("Game", window_capture(658, 34, 562, 1001))
+if cv.waitKey(1) == ord("q"):
+    cv.destroyAllWindows()

92a49426e36bd56f46bb17e282adeb42b77bc210
Kate <45145025+Python-dev-1@users.noreply.github.com>
2021-10-03 22:57:32 -0400

    Update Vision.py

--- Vision.py
+++ Vision.py
@@ -1,56 +1,58 @@
 import cv2 as cv
 import numpy as np
 
-def Vision(haystack, needle):
-    # "D:\\cr.ss\\screen_randon.png"
-    screen = cv.imread(haystack, cv.IMREAD_UNCHANGED)
-    # "D:\\cr.ss\\enemy\\enemy7.png"
-    enemy_lvl = cv.imread(needle, cv.IMREAD_UNCHANGED)
-    needle_w = enemy_lvl.shape[1]
-    needle_h = enemy_lvl.shape[0]
+class Vision():
 
-    result = cv.matchTemplate(screen, enemy_lvl, cv.TM_CCOEFF_NORMED)
+    needle_img = None
+    needle_w = 0
+    needle_h = 0
+    method = None
 
-    threshold = 0.8
-    locations = np.where(result >= threshold)
-    locations = list(zip(*locations[::-1]))
-
-    rectangles = []
-    for loc in locations:
-        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
-        rectangles.append(rect)
-        rectangles.append(rect)
-
-    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
-    print(rectangles)
-
-    if len(rectangles) :
-        line_color = (0, 255, 0)
-        line_type = cv.LINE_4
+    def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
+        self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+        self.needle_w = self.needle_img.shape[1]
+        self.needle_h = self.needle_img.shape[0]
+        self.method = method
 
 
-        for (x, y, w, h) in rectangles:
-            top_left = (x, y)
-            bottom_right = (x + w, y + h)
-            cv.rectangle(screen, top_left, bottom_right, line_color, line_type)
-        cv.imshow("matches 7", screen)
-        cv.waitKey()
+    def find(self, haystack_img, debug_mode=None):
 
+        result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
 
-    points = []
-    if len(rectangles):
+        threshold = 0.783
+        locations = np.where(result >= threshold)
+        locations = list(zip(*locations[::-1]))
+        #print(locations)
 
-        line_color = (0, 255, 0)
-        line_type = cv.LINE_4
-        marker_color = (255, 0, 255)
-        marker_type = cv.MARKER_CROSS
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
+            rectangles.append(rect)
+            rectangles.append(rect)
 
-        # Loop over all the rectangles
-        for (x, y, w, h) in rectangles:
+        rectangles, weights = cv.groupRectangles(rectangles, 1, 0.3)
+        #print(rectangles)
 
-            # Determine the center position
-            center_x = x + int(w/2)
-            center_y = y + int(h*2)
-            # Save the points
-            points.append((center_x, center_y))
-    return points    
+        points = []
+        if len(rectangles):
+
+            line_color = (0, 255, 0)
+            line_type = cv.LINE_4
+            marker_color = (0, 255, 0)
+            marker_type = cv.MARKER_CROSS
+
+            for (x, y, w, h) in rectangles:
+
+                center_x = x + int(w / 2)
+                center_y = y + int(h / 2)
+                points.append((center_x, center_y))
+
+                if debug_mode == "rectangles":
+                    top_left = (x, y)
+                    bottom_tight = (x + w, y + h)
+                    cv.rectangle(haystack_img, top_left, bottom_tight, line_color, line_type)
+                elif debug_mode == "points":
+                    cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
+        if debug_mode:
+            cv.imshow("Matches", haystack_img)
+        return points

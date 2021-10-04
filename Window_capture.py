27b13c6e9ca1f1106b842d0eb3d10beeda99d3e2
Kate <45145025+Python-dev-1@users.noreply.github.com>
2021-10-03 22:58:00 -0400

    Update Window_capture.py

--- Window_capture.py
+++ Window_capture.py
@@ -1,34 +1,54 @@
-import numpy as np
-import warnings
-warnings.simplefilter("ignore", DeprecationWarning)
-import win32gui, win32ui, win32con
 import cv2 as cv
+import numpy as np
+import win32gui
+import win32ui
+import win32con
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def window_capture(x, y, w, h):
+class WindowCapture:
 
-    hwnd = win32gui.FindWindow(None, "LDPlayer")
-    wDC = win32gui.GetWindowDC(hwnd)
-    dcObj = win32ui.CreateDCFromHandle(wDC)
-    cDC = dcObj.CreateCompatibleDC()
-    dataBitMap = win32ui.CreateBitmap()
-    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-    cDC.SelectObject(dataBitMap)
-    cDC.BitBlt((0, 0), (w, h), dcObj, (x, y), win32con.SRCCOPY)
+    w = 0
+    h = 0
+    hwnd = None
+    offset_x = 0
+    offset_y = 0
 
-    signedIntsArray = dataBitMap.GetBitmapBits(True)
-    img = np.fromstring(signedIntsArray, dtype="uint8")
-    img.shape = (h, w, 4)
-    img = img[...,:3]
-    img = np.ascontiguousarray(img)
+    def __init__(self, window_name):
 
-    # Free Resources
-    dcObj.DeleteDC()
-    cDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, wDC)
-    win32gui.DeleteObject(dataBitMap.GetHandle())
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception("Window not found: {}".format(window_name))
 
-    return img
+        self.w = 561
+        self.h = 1003
 
-cv.imshow("Game", window_capture(658, 34, 562, 1001))
-if cv.waitKey(1) == ord("q"):
-    cv.destroyAllWindows()
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+
+        self.offset_x = window_rect[0] + 658
+        self.offset_y = window_rect[1] + 34
+
+    def get_screenshot(self):
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj=win32ui.CreateDCFromHandle(wDC)
+        cDC=dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0,0),(self.w, self.h) , dcObj, (658,34), win32con.SRCCOPY)
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype="uint8")
+        img.shape = (self.h, self.w, 4)
+
+        # Free Resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        #img = np.ascontiguousarray(img)
+        return img
+
+
+        def get_screen_position(self, pos):
+            return (pos[0] + self.offset_x, pos[1] + self.offset_y)
